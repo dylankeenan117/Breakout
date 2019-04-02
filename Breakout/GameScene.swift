97dfc0f93f8ab3dff0ba3a1619651404cbd073a3
Dylan Keenan <dylankeenan&icloud.com>
2019-04-02 11:53:39 -0500

    Added motion paddle dynamic

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
@@ -17,81 +17,117 @@ class GameScene: SKScene {
     var loseZone = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        createBackground()
+        makeBall()
+        makePaddle()
+        makeBrick()
+        makeLoseZone()
+        
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
         
     }
     
     func createBackground(){
         let stars = SKTexture(imageNamed: "stars")
         for i in 0...1 {
-            func createBackground() {
-                let stars = SKTexture(imageNamed: "stars")
-                for i in 0...1 {
-                    let starsBackground = SKSpriteNode(texture: stars)
-                    starsBackground.zPosition = -1
-                    starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
-                    addChild(starsBackground)
-                    let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
-                    let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
-                    let moveLoop = SKAction.sequence([moveDown, moveReset])
-                    let moveForever = SKAction.repeatForever(moveLoop)
-                    starsBackground.run(moveForever)
-                }
-            }
-        }
-        func makeBall() {
-            ball = SKShapeNode(circleOfRadius: 10)
-            ball.position = CGPoint(x: frame.midX, y: frame.midY)
-            ball.strokeColor = UIColor.black
-            ball.fillColor = UIColor.yellow
-            ball.name = "ball"
             
-            // physics shape matches ball image
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
-            // ignores all forces and impulses
-            ball.physicsBody?.isDynamic = false
-            // use precise collision detection
-            ball.physicsBody?.usesPreciseCollisionDetection = true
-            // no loss of energy from friction
-            ball.physicsBody?.friction = 0
-            // gravity is not a factor
-            ball.physicsBody?.affectedByGravity = false
-            // bounces fully off of other objects
-            ball.physicsBody?.restitution = 1
-            // does not slow down over time
-            ball.physicsBody?.linearDamping = 0
-            ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
-            
-            addChild(ball) // add ball object to the view
+            let starsBackground = SKSpriteNode(texture: stars)
+            starsBackground.zPosition = -1
+            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+            addChild(starsBackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            starsBackground.run(moveForever)
         }
+    }
+    func makeBall() {
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = UIColor.black
+        ball.fillColor = UIColor.yellow
+        ball.name = "ball"
         
-        func makePaddle() {
-            paddle = SKSpriteNode(color: UIColor.white, size: CGSize(width: frame.width/4, height: 20))
-            paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
-            paddle.name = "paddle"
-            paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
-            paddle.physicsBody?.isDynamic = false
-            addChild(paddle)
-        }
+        // physics shape matches ball image
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        // ignores all forces and impulses
+        ball.physicsBody?.isDynamic = false
+        // use precise collision detection
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        // no loss of energy from friction
+        ball.physicsBody?.friction = 0
+        // gravity is not a factor
+        ball.physicsBody?.affectedByGravity = false
+        // bounces fully off of other objects
+        ball.physicsBody?.restitution = 1
+        // does not slow down over time
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
         
-        func makeBrick() {
-            brick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 50, height: 20))
-            brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
-            brick.name = "brick"
-            brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
-            brick.physicsBody?.isDynamic = false
-            addChild(brick)
+        addChild(ball) // add ball object to the view
+    }
+    
+    func makePaddle() {
+        paddle = SKSpriteNode(color: UIColor.white, size: CGSize(width: frame.width/4, height: 20))
+        paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+        paddle.name = "paddle"
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        addChild(paddle)
+    }
+    
+    func makeBrick() {
+        brick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
+    }
+    func makeLoseZone() {
+        loseZone = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width, height: 50))
+        loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
+        loseZone.name = "loseZone"
+        loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+        loseZone.physicsBody?.isDynamic = false
+        addChild(loseZone)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
         }
-        func makeLoseZone() {
-            loseZone = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width, height: 50))
-            loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
-            loseZone.name = "loseZone"
-            loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
-            loseZone.physicsBody?.isDynamic = false
-            addChild(loseZone)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "brick" ||
+            contact.bodyB.node?.name == "brick" {
+            print("You win!")
+            brick.removeFromParent()
+            ball.removeFromParent()
+        }
+        if contact.bodyA.node?.name == "loseZone" ||
+            contact.bodyB.node?.name == "loseZone" {
+            print("You lose!")
+            ball.removeFromParent()
         }
     }
 }
-        
-    
+
+
+
+
 
 
